aa83cd88dea61595caf4dfb744abd5ceffa49784
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 17:15:53 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+#Define methods that use iteration and control the return values of those methods.
+#Define methods that call other methods.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+
+
+def batch_badge_creator(speakers)
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+speakers.each do |speaker|
+Puts "Hello, my name is #{speaker}"
+ end 
+end
+
+
+def assign_rooms(speakers)
+  speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"] 
+  arr = []
+  room = 1
+  speakers.each do { |speaker| arr << "#{speaker = room + 1}" }
+  room += 1
+  end
+  puts "Hello #{speaker}! You'll be assigned to room #{arr}"
+  
+end
+
+def printer
+  print batch_badge_creator
+  print assign_rooms
+end
